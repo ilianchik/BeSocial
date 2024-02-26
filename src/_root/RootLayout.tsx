e57bbce7027d402490368dd7513626b69a7b2fd5
Ilia Kozachyshyn <kozachishin543@gmail.com>
2024-02-26 17:04:13 +0200

    v-1

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+
+const RootLayout = () => {
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 min-h-[100%]">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
